8d592e4174e0b57c5788b2f75a8ce0d4f53e6acc
Elena Garcia Herrero <egarciah@minsait.com>
2021-11-09 21:39:45 +0100

    First commit with demo of springBoot

---
+++ src/test/java/garcia/herrero/MartianRobots/MartianRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package garcia.herrero.MartianRobots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartianRobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
