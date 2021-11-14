4e1ee664690eee90f28927bb05c74b17629b1df9
Elena Garcia Herrero <egarciah@minsait.com>
2021-11-14 23:02:44 +0100

    start and stop scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t elenagherrero/martian_robots:0.0.1 .
-docker run -p 8080:8080 elenagherrero/martian_robots:0.0.1
+docker run -p 8080:8080 --name martinrobots elenagherrero/martian_robots:0.0.1
